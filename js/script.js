a9870fa503be75aeb0581f96aa2b8c53e5093172
MatteoMeneguzzi <meneguzzi.matteo16@gmail.com>
2021-04-13 15:25:22 +0200

    complete

--- js/script.js
+++ js/script.js
@@ -5,12 +5,61 @@
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 // Una volta creato il nuovo oggetto inserirlo nell'array creato al punto 2 e mostrare alla fine il contenuto dell'array.
 
-var student = {
-	nome: ["Mario"],
-	cognome: ["Rossi"],
-	eta: ["19"],
+// var student = {
+// 	nome: ["Mario"],
+// 	cognome: ["Rossi"],
+// 	eta: ["19"],
+// };
+
+// for (var key in student) {
+// 	// console.log(key, student[key]);
+// }
+
+var classe = [
+	{
+		nome: ["Mario"],
+		cognome: ["Rossi"],
+		eta: ["19"],
+	},
+	{
+		nome: ["Albert"],
+		cognome: ["Einstein"],
+		eta: ["20"],
+	},
+	{
+		nome: ["Diego"],
+		cognome: ["Maradona"],
+		eta: ["18"],
+	},
+	{
+		nome: ["Barack"],
+		cognome: ["Obama"],
+		eta: ["22"],
+	},
+	{
+		nome: ["Steve"],
+		cognome: ["Jobs"],
+		eta: ["19"],
+	},
+	{
+		nome: ["Dante"],
+		cognome: ["Alighieri"],
+		eta: ["19"],
+	},
+];
+
+var newStudent = {
+	nome: prompt("Inserisci il tuo nome").trim(),
+	cognome: prompt("Inserisci il tuo cognome").trim(),
+	eta: parseInt(prompt("Inserisci la tua età").trim()),
 };
 
-for (var key in student) {
-	console.log(student[key]);
+classe.push(newStudent);
+
+for (var i = 0; i < classe.length; i++) {
+	var student = classe[i];
+
+	// console.log(student);
+	console.log(student.nome);
+	console.log(student.cognome);
 }
