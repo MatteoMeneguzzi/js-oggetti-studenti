bed27a9b467c7dd6f7b53a9cc894c9c61987f990
MatteoMeneguzzi <meneguzzi.matteo16@gmail.com>
2021-04-13 14:51:41 +0200

    files

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// 	Descrizione
+// Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+// Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+// Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+// Una volta creato il nuovo oggetto inserirlo nell'array creato al punto 2 e mostrare alla fine il contenuto dell'array.
+
+var student = {
+	nome: ["Mario"],
+	cognome: ["Rossi"],
+	eta: ["19"],
+};
+
+for (var key in student) {
+	console.log(student[key]);
+}
